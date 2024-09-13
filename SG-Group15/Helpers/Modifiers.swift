f9145fc8946003fafef3f694636ee5ef5e895914
Xian <hongvanct2003@gmail.com>
2024-09-13 21:44:02 +0700

    add modifiers

--- SG-Group15/Helpers/Modifiers.swift
+++ SG-Group15/Helpers/Modifiers.swift
@@ -16,17 +16,62 @@ struct LargeButtonModifier: ViewModifier {
     func body(content: Content) -> some View {
         content
         // Responsive frame
-            .frame(minWidth: UIScreen.main.bounds.width * 0.3, maxWidth: UIScreen.main.bounds.width * 0.4)
-            .fontWeight(.bold)
-            .font(.system(size: UIScreen.main.bounds.width * 0.05))
-            .foregroundStyle(.textLight)
+            .frame(minWidth: UIScreen.main.bounds.width * 0.45, maxWidth: UIScreen.main.bounds.width * 0.5)
+            .font(.custom("Lato-Black", size: UIScreen.main.bounds.width * 0.06))
+            .foregroundStyle(.white)
             .padding(.vertical, 10)
             .background(background)
             .cornerRadius(15)
         // Ajust shadow to be responsive
-            .shadow(radius: 2, x: 1, y: UIScreen.main.bounds.width * 0.01)
+            .shadow(radius: 2, x: 1, y: UIScreen.main.bounds.width * 0.015)
     }
 }
+
+// Medium button style: Add to the label
+struct MediumButtonModifier: ViewModifier {
+    // Add background color as parameter
+    var background: Color
+    func body(content: Content) -> some View {
+        content
+        // Responsive frame
+            .frame(minWidth: UIScreen.main.bounds.width * 0.3, maxWidth: UIScreen.main.bounds.width * 0.4)
+            .font(.custom("Lato-Black", size: UIScreen.main.bounds.width * 0.5))
+            .foregroundStyle(.white)
+            .padding(.vertical)
+            .background(background)
+            .cornerRadius(15)
+        // Ajust shadow to be responsive
+            .shadow(radius: 2, x: 1, y: UIScreen.main.bounds.width * 0.015)
+    }
+}
+
+// Title text style: Apply for book title, event title and chapter title
+struct TitleTextModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.custom("OldStandardTT-Bold", size: UIScreen.main.bounds.width * 0.06))
+            .foregroundStyle(.textDark)
+    }
+}
+
+// Chapter text style: Apply for book details' overview in the front page
+struct HeadlineTextModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.custom("Lato-Light", size: UIScreen.main.bounds.width * 0.05))
+            .foregroundStyle(.textDark)
+    }
+}
+
+// Body text style: Apply for conten in answers
+struct BodyTextModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.custom("Lato-Regular", size: UIScreen.main.bounds.width * 0.045))
+            .foregroundStyle(.textDark)
+    }
+}
+
 
 
 
