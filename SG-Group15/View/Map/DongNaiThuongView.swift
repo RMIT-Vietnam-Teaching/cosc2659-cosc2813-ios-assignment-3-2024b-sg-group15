408d95614c1405eab0806873b4a0ce3f094e5138
kieuahn <s3818552@rmit.edu.vn>
2024-09-15 19:06:48 +0700

    DongNaiThuongView position fixed

---
+++ SG-Group15/View/Map/DongNaiThuongView.swift
@@ -0,0 +1,18 @@
+//
+//  DongNaiThuongView.swift
+//  SG-Group15
+//
+//  Created by Nguyen Ha Kieu Anh on 15/9/24.
+//
+
+import SwiftUI
+
+struct DongNaiThuongView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DongNaiThuongView()
+}
---
+++ SG-Group15/View/MapComponent/MapComponent4.swift
@@ -0,0 +1,8 @@
+//
+//  MapComponent4.swift
+//  SG-Group15
+//
+//  Created by Nguyen Ha Kieu Anh on 15/9/24.
+//
+
+import Foundation
