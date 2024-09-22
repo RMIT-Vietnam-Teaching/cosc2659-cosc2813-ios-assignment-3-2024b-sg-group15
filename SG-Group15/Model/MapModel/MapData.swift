2864196d8dbad16b41e409c091a130c82077aee4
vntuananh <vntuananhbui@gmail.com>
2024-09-22 13:17:28 +0700

    Initialize Map Game Firestore

--- SG-Group15/Model/MapModel/MapData.swift
+++ SG-Group15/Model/MapModel/MapData.swift
@@ -22,3 +22,28 @@ struct MapComponentConfig {
     let animationPosition: (compact: CGPoint, regular: CGPoint)
     let animationOffset: CGSize
 }
+
+import Foundation
+
+// Model used for Map question
+struct MapQuestion: Identifiable, QuestionProtocol {
+    var id: String
+    var question: String
+    var mapChoices: [String]
+    var correct: String
+    var questionType: QuestionType = .map
+    
+    // Initialize from Firestore document ID and data
+    init?(documentID: String, data: [String: Any]) {
+        guard let question = data["mapQuestion"] as? String,
+              let mapChoices = data["mapChoices"] as? [String],
+              let correct = data["correct"] as? String else {
+            return nil
+        }
+        
+        self.id = documentID
+        self.question = question
+        self.mapChoices = mapChoices
+        self.correct = correct
+    }
+}
