31cd52285321d20bcc1a95054388135d2b62012b
vntuananh <vntuananhbui@gmail.com>
2024-09-22 13:06:04 +0700

    Update file name

---
+++ SG-Group15/View/Map/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  MapView.swift
+//  SG-Group15
+//
+//  Created by Tuan Anh Bui on 9/22/24.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapView()
+}
